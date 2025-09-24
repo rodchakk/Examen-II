20dbc8a4123a553293631cd4311523b62d62068a
Rodolfo Chacon <rudychacon01@gmail.com>
2025-09-23 20:56:41 -0600

    testing

--- src/hooks/useEmployee.js
+++ src/hooks/useEmployee.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const API_URL = "https://674c84c054e1fca9290cd05f.mockapi.io/api/examen/empleado";
+// const API_URL = "https://674c84c054e1fca9290cd05f.mockapi.io/api/examen/empleado";
 
-export default function useEmployee() {   
-  const [employees, setEmployees] = useState([]);
+// export default function useEmployee() {   
+//   const [employees, setEmployees] = useState([]);
 
-  // GET empleados
-  useEffect(() => {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(data => setEmployees(data));
-  }, []);
+//   // GET empleados
+//   useEffect(() => {
+//     fetch(API_URL)
+//       .then(res => res.json())
+//       .then(data => setEmployees(data));
+//   }, []);
 
-  // POST empleados
-  const addEmployee = async (employee) => {
-    const res = await fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(employee),
-    });
-    const newEmployee = await res.json();
-    setEmployees([...employees, newEmployee]);
-  };
+//   // POST empleados
+//   const addEmployee = async (employee) => {
+//     const res = await fetch(API_URL, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(employee),
+//     });
+//     const newEmployee = await res.json();
+//     setEmployees([...employees, newEmployee]);
+//   };
 
-  return { employees, addEmployee };
-}
+//   return { employees, addEmployee };
+// }
